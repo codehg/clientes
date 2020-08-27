0640a414cc56ba2b2a3c8814f99d47aad8fff0eb
codehg <hugoaraliveira@gmail.com>
2020-08-27 14:03:36 -0300

    Add Methods Get Post Del Put

--- src/main/java/io/github/codehg/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/codehg/clientes/model/entity/Cliente.java
@@ -1,12 +1,23 @@
 package io.github.codehg.clientes.model.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
 
     @Id
@@ -14,12 +25,21 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull
+    @CPF
     private String cpf;
 
-    @Column(name = "data_cadastro")
+    @Column(name = "data_cadastro", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        setDataCadastro(LocalDate.now());
+    }
 
 }
